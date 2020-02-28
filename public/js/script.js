545d0e6d8a3c8d8826974ed45090173def5fb82d
Joelton Lino Luz <joelton@live.com>
2020-02-27 22:46:58 -0300

    :rocket: add new functions to add ingredient/remove

--- public/js/script.js
+++ public/js/script.js
@@ -23,3 +23,45 @@ for(let link of linkHideShow) {
     };
   });
 };
+
+function addInput(divPai, divFilho) {
+  const divPrincipal = document.querySelector(`${divPai}`);
+  const fieldContainer = document.querySelectorAll(`${divFilho}`);
+  
+  // Realiza um clone do último ingrediente adicionado
+  const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+  
+  // Não adiciona um novo input se o último tem um valor vazio
+  if (newField.children[1].value == "") {
+    return false;
+  };
+
+  // Deixa o valor do input vazio
+  console.log(newField.children[1].value);
+  newField.children[1].value = '';
+  divPrincipal.appendChild(newField);
+};
+
+function removeInput(line) { 
+  //Verificar se tem apenas um ingredient
+  const divPai = line.parentNode.parentNode;    
+  if (divPai.children.length==1) {
+    return alert('Mínimo permitido!!');
+  };
+
+  if (confirm('Deseja remover essa Linha?')) {  
+    line.parentNode.remove();
+  };
+};
+
+document
+  .querySelector('.addNewIngredient')
+  .addEventListener('click',() => {
+    addInput('#ingredients','.ingredient');
+  });
+
+document
+  .querySelector('.addNewPreparation')
+  .addEventListener('click', () => {
+    addInput('#preparations','.preparation');
+  });
